759180a0240c56ddefda24b0ad431a05b2d87987
Valeria Franciscangeli <vvfranciscangeli@gmail.com>
2022-03-31 11:02:49 -0300

    se ordena de mejor manera metodo main para mostrar ejemplos del funcionamiento

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,16 @@ public class Main {
         System.out.println("trabajo02: "+trabajo02);
         System.out.println("trabajo03: "+trabajo03);
         System.out.println("trabajo04: "+trabajo04);
-        //System.out.println("los trabajos 1 y 4 son iguales?:"+ trabajo01.equals(trabajo04));
+
+        System.out.println("COMPARAMOS IGUALDADES ==============================");
+
+        System.out.println("trabajo01 es igual a trabajo02?: " +  trabajo01.equals(trabajo02)); //false
+        System.out.println("trabajo02 es igual a trabajo01?: " +  trabajo02.equals(trabajo01)); //false
+        System.out.println("trabajo03 es igual a trabajo03?: " +  trabajo03.equals(trabajo03)); //true
+        System.out.println("trabajo01 es igual a trabajo04?: " +  trabajo01.equals(trabajo04)); //true
+
+        System.out.println();
+
         //####################################################
         System.out.println("LITERARY WORK TEST==============================");
         LiteraryWork trabajo05 = new LiteraryWork("El juego de Ripper","Sudamericana", "Derechos Reservados");
@@ -31,6 +40,16 @@ public class Main {
         System.out.println("trabajo06: "+trabajo06);
         System.out.println("trabajo07: "+trabajo07);
         System.out.println("trabajo08: "+trabajo08);
+
+        System.out.println("COMPARAMOS IGUALDADES ==============================");
+
+        System.out.println("trabajo05 es igual a trabajo06?: " +  trabajo05.equals(trabajo06)); //false
+        System.out.println("trabajo07 es igual a trabajo05?: " +  trabajo07.equals(trabajo05)); //true
+        System.out.println("trabajo07 es igual a trabajo07?: " +  trabajo07.equals(trabajo07)); //true
+        System.out.println("trabajo05 es igual a trabajo08?: " +  trabajo05.equals(trabajo08)); //false
+
+        System.out.println();
+
         //####################################################
         System.out.println("MOVIE TEST==============================");
         String[] cast1= new String[]{"Max Schimmelpfennig","Lea van Acken", "Tijan Marei"};
@@ -46,6 +65,16 @@ public class Main {
         System.out.println("trabajo10: "+trabajo10);
         System.out.println("trabajo11: "+trabajo11);
         System.out.println("trabajo12: "+trabajo12);
+
+        System.out.println("COMPARAMOS IGUALDADES ==============================");
+
+        System.out.println("trabajo09 es igual a trabajo10?: " +  trabajo09.equals(trabajo10)); //false
+        System.out.println("trabajo10 es igual a trabajo09?: " +  trabajo10.equals(trabajo09)); //false
+        System.out.println("trabajo11 es igual a trabajo11?: " +  trabajo11.equals(trabajo11)); //true
+        System.out.println("trabajo09 es igual a trabajo12?: " +  trabajo09.equals(trabajo12)); //false
+
+        System.out.println();
+
         //####################################################
         System.out.println("NOVEL TEST==============================");
         Novel trabajo13 = new Novel("Malaventura", "Impedimenta", "Fernando Navarro");
@@ -58,6 +87,16 @@ public class Main {
         System.out.println("trabajo14: "+trabajo14);
         System.out.println("trabajo15: "+trabajo15);
         System.out.println("trabajo16: "+trabajo16);
+
+        System.out.println("COMPARAMOS IGUALDADES ==============================");
+
+        System.out.println("trabajo13 es igual a trabajo14?: " +  trabajo13.equals(trabajo14)); //false
+        System.out.println("trabajo14 es igual a trabajo13?: " +  trabajo14.equals(trabajo13)); //false
+        System.out.println("trabajo15 es igual a trabajo15?: " +  trabajo15.equals(trabajo15)); //true
+        System.out.println("trabajo13 es igual a trabajo16?: " +  trabajo13.equals(trabajo16)); //false
+
+        System.out.println();
+
         //####################################################
         System.out.println("COMIC TEST==============================");
         String[] cartoonists1= new String[]{"Gabriel Bá","Dave Stewart", "Nick Filardi"};
@@ -74,63 +113,39 @@ public class Main {
         System.out.println("trabajo18: "+trabajo18);
         System.out.println("trabajo19: "+trabajo19);
         System.out.println("trabajo20: "+trabajo20);
-        //####################################################
 
         System.out.println("COMPARAMOS IGUALDADES ==============================");
 
-        System.out.println("trabajo01 es igual a trabajo02?: " +  trabajo01.equals(trabajo02));
-        System.out.println("trabajo02 es igual a trabajo01?: " +  trabajo02.equals(trabajo01));
-        System.out.println("trabajo03 es igual a trabajo03?: " +  trabajo03.equals(trabajo03));
-        System.out.println("trabajo01 es igual a trabajo04?: " +  trabajo01.equals(trabajo04));
+        System.out.println("trabajo17 es igual a trabajo18?: " +  trabajo17.equals(trabajo18)); //false
+        System.out.println("trabajo18 es igual a trabajo17?: " +  trabajo18.equals(trabajo17)); //false
+        System.out.println("trabajo19 es igual a trabajo19?: " +  trabajo19.equals(trabajo19)); //true
+        System.out.println("trabajo18 es igual a trabajo20?: " +  trabajo18.equals(trabajo20)); //true
 
-        System.out.println("==============================");
-
-        System.out.println("trabajo05 es igual a trabajo06?: " +  trabajo05.equals(trabajo06));
-        System.out.println("trabajo06 es igual a trabajo05?: " +  trabajo06.equals(trabajo05));
-        System.out.println("trabajo07 es igual a trabajo07?: " +  trabajo07.equals(trabajo07));
-        System.out.println("trabajo05 es igual a trabajo08?: " +  trabajo05.equals(trabajo08));
-
-        System.out.println("==============================");
-
-        System.out.println("trabajo09 es igual a trabajo10?: " +  trabajo09.equals(trabajo10));
-        System.out.println("trabajo10 es igual a trabajo09?: " +  trabajo10.equals(trabajo09));
-        System.out.println("trabajo11 es igual a trabajo11?: " +  trabajo11.equals(trabajo11));
-        System.out.println("trabajo09 es igual a trabajo12?: " +  trabajo09.equals(trabajo12));
-
-        System.out.println("==============================");
-
-        System.out.println("trabajo13 es igual a trabajo14?: " +  trabajo13.equals(trabajo14));
-        System.out.println("trabajo14 es igual a trabajo13?: " +  trabajo14.equals(trabajo13));
-        System.out.println("trabajo15 es igual a trabajo15?: " +  trabajo15.equals(trabajo15));
-        System.out.println("trabajo13 es igual a trabajo16?: " +  trabajo13.equals(trabajo16));
-
-        System.out.println("==============================");
-
-        System.out.println("trabajo17 es igual a trabajo18?: " +  trabajo17.equals(trabajo18));
-        System.out.println("trabajo18 es igual a trabajo17?: " +  trabajo18.equals(trabajo17));
-        System.out.println("trabajo19 es igual a trabajo19?: " +  trabajo19.equals(trabajo19));
-        System.out.println("trabajo18 es igual a trabajo20?: " +  trabajo18.equals(trabajo20));
-
-        System.out.println("==============================");
-        
-        System.out.println("trabajo03 es igual a trabajo10?: " +  trabajo03.equals(trabajo10));
-        System.out.println("trabajo08 es igual a trabajo19?: " +  trabajo08.equals(trabajo19));
-        System.out.println("trabajo09 es igual a trabajo09?: " +  trabajo09.equals(trabajo09));
-        System.out.println("trabajo04 es igual a trabajo10?: " +  trabajo04.equals(trabajo10));
-        System.out.println("trabajo20 es igual a trabajo18?: " +  trabajo20.equals(trabajo18));
-        System.out.println("trabajo02 es igual a trabajo12?: " +  trabajo02.equals(trabajo12));
-        System.out.println("trabajo09 es igual a trabajo09?: " +  trabajo09.equals(trabajo09));
-        System.out.println("trabajo01 es igual a trabajo20?: " +  trabajo01.equals(trabajo20));
+        System.out.println();
 
         //####################################################
 
-        System.out.println(" Revisamos si tienen copyright ==============================");
-        System.out.println("trabajo01 tiene copyright?:"+ trabajo01.hasCopyright());
-        System.out.println("trabajo08 tiene copyright?:"+ trabajo08.hasCopyright());
-        System.out.println("trabajo10 tiene copyright?:"+ trabajo10.hasCopyright());
-        System.out.println("trabajo07 tiene copyright?:"+ trabajo07.hasCopyright());
-        System.out.println("trabajo18 tiene copyright?:"+ trabajo18.hasCopyright());
-        System.out.println("trabajo13 tiene copyright?:"+ trabajo13.hasCopyright());
+        System.out.println("PRUEBAS ALEATORIAS ENTRE SUBCLASES==============================");
+
+        System.out.println("COMPARAMOS IGUALDADES==============================");
+        System.out.println("trabajo03 es igual a trabajo10?: " +  trabajo03.equals(trabajo10)); //false
+        System.out.println("trabajo08 es igual a trabajo19?: " +  trabajo08.equals(trabajo19)); //false
+        System.out.println("trabajo09 es igual a trabajo09?: " +  trabajo09.equals(trabajo09)); //true
+        System.out.println("trabajo04 es igual a trabajo10?: " +  trabajo04.equals(trabajo10)); //false
+        System.out.println("trabajo20 es igual a trabajo18?: " +  trabajo20.equals(trabajo18)); //true
+        System.out.println("trabajo02 es igual a trabajo12?: " +  trabajo02.equals(trabajo12)); //false
+        System.out.println("trabajo09 es igual a trabajo09?: " +  trabajo09.equals(trabajo09)); //true
+        System.out.println("trabajo01 es igual a trabajo20?: " +  trabajo01.equals(trabajo20)); //false
+
+        //####################################################
+
+        System.out.println(" REVISAMOS SI TIENEN COPYRIGHT ==============================");
+        System.out.println("trabajo01 tiene copyright?:"+ trabajo01.hasCopyright()); //false
+        System.out.println("trabajo08 tiene copyright?:"+ trabajo08.hasCopyright()); //false
+        System.out.println("trabajo10 tiene copyright?:"+ trabajo10.hasCopyright()); //false
+        System.out.println("trabajo07 tiene copyright?:"+ trabajo07.hasCopyright()); //true
+        System.out.println("trabajo18 tiene copyright?:"+ trabajo18.hasCopyright()); //true
+        System.out.println("trabajo13 tiene copyright?:"+ trabajo13.hasCopyright()); //false
 
 
     }
